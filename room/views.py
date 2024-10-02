dac730203c447c83bf9ec1d25cdaa900d9321565
Huu Phuoc <xitet0007@gmail.com>
2024-10-02 18:01:23 +0700

    pussh room

--- room/views.py
+++ room/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is room")
+    return HttpResponse("room.html")
 
 def index1(request):
     return HttpResponse("this is room1")
