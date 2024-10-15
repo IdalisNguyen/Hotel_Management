fe5031481c0837156e465faa9768e450e0e4a940
Huu Phuoc <xitet0007@gmail.com>
2024-10-15 17:23:21 +0700

    add notifications vs update view booking room

--- room/models.py
+++ room/models.py
@@ -1,6 +1,9 @@
 # room/models.py
 from django.db import models
 from django.contrib.auth import get_user_model  # This is to get the custom User model
+from django.contrib.auth.models import User
+
+User = get_user_model()
 
 class RoomType(models.Model):
     name = models.CharField(max_length=255)
@@ -29,10 +32,19 @@ class Room(models.Model):
     
 
 class RoomBooking(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, blank=True, default=2)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     date_start = models.DateTimeField()
     date_end = models.DateTimeField()
+    phone = models.CharField(max_length=15, blank=True, null=True)  # Để trống ban đầu
+    email = models.EmailField()  # Email vẫn bắt buộc
+    message = models.TextField(blank=True, null=True)  # Optional
+
+    def save(self, *args, **kwargs):
+        # Tự động lấy email từ người dùng trước khi lưu
+        if self.user:
+            self.email = self.user.email  # Lấy email từ User
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Booking of {self.room.name} by {self.user.username} from {self.date_start} to {self.date_end}"
+        return f"Booking by {self.user} for room {self.room} from {self.date_start} to {self.date_end}"
