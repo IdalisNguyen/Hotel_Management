e8c87b927db414ae74c31128e29941fd38ffca14
Huu Phuoc <xitet0007@gmail.com>
2024-11-13 14:49:51 +0700

    merge anhnhan branch

--- service/urls.py
+++ service/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('<int:roombooking_id>/', views.service, name='service'),
     path('order_food/<int:roombooking_id>/<int:food_id>/', views.order_food, name='order_food'),
     path('delete_food_order/<int:roombooking_id>/<int:order_id>/', views.delete_food_order, name='delete_food_order'),
+    path('food_order_detail/<int:roombooking_id>/<int:food_id>/', views.food_order_detail, name='food_order_detail'),
+    path('update_food_order/<int:roombooking_id>/<int:order_id>/', views.update_food_order, name='update_food_order'),
     path('vehicle_details/',views.order_vehicle,name = "vehicle_details")
 
 ]
